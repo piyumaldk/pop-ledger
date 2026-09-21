037291441dcc7dad43c60cbc37d92f2969924095
piyumaldk <piyumaldk@gmail.com>
2026-09-21 11:22:11 +0530

    Revert "Fix Header Issue"
    This reverts commit b3d74d0752a8a0e6ed8e235afe3293e597cc4f94.

--- src/App.tsx
+++ src/App.tsx
@@ -233,7 +233,7 @@ export default function App() {
               </Box>
 
               {/* Theme toggle + user */}
-              <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5, ml: 'auto' }}>
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
 
                 <ButtonBase
                   onClick={handleMenuOpen}
